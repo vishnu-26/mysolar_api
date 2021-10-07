00f1d21025cdd7d4bc7389c99a047a8ba53f61ca
vishnu-26 <vishnu26@gmail.com>
2021-10-07 11:36:52 +0530

    Allowed Cors Origin

--- api/settings.py
+++ api/settings.py
@@ -42,11 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-#    'corsheaders',
+    'corsheaders',
     'products'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
